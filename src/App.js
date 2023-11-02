489ef4d19f32d734f837d7838378ec0dbef6a09b
itskhan34 <muzahid34@gmail.com>
2023-11-02 20:06:02 +0600

    VG

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import Countries from './components/Countries/Countries';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countries></Countries>
+
+
     </div>
   );
 }
+
+// function LoadCountry() {
+//   const [countries, setCountry] = useState([])
+//   useEffect(() => {
+//     fetch("https://restcountries.com/v3.1/all")
+//       .then(res => res.json())
+//       .then(data => setCountry(data))
+
+//   }, [])
+//   return (
+//     <div>
+//       <h1>World tour</h1>
+//       <h3>All countries :{countries.length}</h3>
+//       {
+//         countries.map(country => <Country name={country.name.common} capital={country.capital} population={country.population} region={country.region}></Country>)
+//       }
+
+//     </div>
+//   )
+// }
+// function Country(props) {
+//   return (
+//     <div>
+//       <h2>Name: {props.name}</h2>
+//       <h3>Capital: {props.capital}</h3>
+//       <h4>Region: {props.region}</h4>
+//       <h4>population: {props.population}</h4>
+
+
+//     </div>
+//   )
+// }
 
 export default App;
---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,24 @@
+
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+import './Countries.css';
+const Countries = () => {
+    const [countries, setcountries] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setcountries(data))
+    }, [])
+    return (
+        <div >
+            <h1>Hello from countries: {countries.length}</h1>
+            <div className='countries-con'>
+                {
+                    countries.map(country => <Country country={country} key={country.cca3}></Country>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Countries;
